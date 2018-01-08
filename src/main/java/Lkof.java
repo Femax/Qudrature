701af694f85347964a94c9d5bff3b21b45d99e90
Femax <femax339@gmail.com>
2018-01-08 10:36:33 +0300

    stage

--- src/main/java/Lkof.java
+++ src/main/java/Lkof.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
+
 /**
  * Created by Max on 27.12.2017.
  */
 public class Lkof {
     private float x1, x2, x3, x0;
 
-    public Lkof(float x1, float x2, float x3, float x0) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.x0 = x0;
+    public static void main(String... args) {
+        float[] x = new float[]{-0.96816f, -0.836031f, -0.613371f, -0.324253f, 0, 0.324253f, 0.613371f, 0.836031f, 0.96816f};
+        float sum = 0;
+        float[] ai = new float[x.length];
+        for (int i = 0; i < x.length; i++) {
+            ai[i] = (float) (2 / ((1 - Math.pow(x[i], 2)) * Math.pow(legandra(x[i]), 2)));   //<---
+        }
+        System.out.println("ai" + Arrays.toString(ai));
+        System.out.println("x" + Arrays.toString(x));
     }
 
-    public float calculate(float x) {
-        return (x - x1) * (x - x2) * (x - x3) / ((x0 - x1) * (x0 - x2) * (x0 - x3));
+    private static float legandra(float x) {
+        return (float) ((float)45 / 128 * (7 - 308 * Math.pow(x, 2) + 2002 * Math.pow(x, 4) - 4004 * Math.pow(x, 6) + 2431 * Math.pow(x, 8)));
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,10 @@ public class Main {
         float integral = 0;
         float[] x = getMassiveX(leftx, rightx, step);
         float[] y = getMassiveY(x);
-        System.out.println("n = 0 : integral = " + square(x, y, leftx, rightx));
-        System.out.println("n = 1 : integral = " + trapeze(x, y, leftx, rightx));
-        System.out.println("n = 2 : integral = " + simpson(x, y, leftx, rightx));
-        System.out.println("Legandra: " + methodlegandra(leftx, rightx));
+        System.out.println("Ньютона-Котеса n = 0 :  " + n1(x, y, leftx, rightx));
+        System.out.println("Ньютона-Котеса n = 1 :  " + n2(x, y, leftx, rightx));
+        System.out.println("Ньютона-Котеса n = 2 :  " + n3(x, y, leftx, rightx));
+        System.out.println("Лежандра: " + methodlegandra(leftx, rightx));
     }
 
     private static float[] getMassiveY(float[] x) {
@@ -55,7 +55,7 @@ public class Main {
         return Math.abs(x);
     }
 
-    public static float square(float[] x, float[] y, float a, float b) {
+    public static float n1(float[] x, float[] y, float a, float b) {
         float sum = 0;
         for (int i = 0; i < y.length; i++) {
             sum = sum + y[i];
@@ -63,7 +63,7 @@ public class Main {
         return (b - a) / x.length * sum;
     }
 
-    public static float trapeze(float[] x, float[] y, float a, float b) {
+    public static float n2(float[] x, float[] y, float a, float b) {
         float sum = 0;
         float h = x[1] - x[0];
         for (int i = 1; i < y.length - 1; i++) {
@@ -72,7 +72,7 @@ public class Main {
         return h * ((y[0] + y[y.length - 1]) / 2 + sum);
     }
 
-    public static float simpson(float[] x, float[] y, float a, float b) {
+    public static float n3(float[] x, float[] y, float a, float b) {
         float sum = 0;
         float h = x[1] - x[0];
         sum += y[0] + y[y.length - 1];
@@ -95,39 +95,35 @@ public class Main {
 //        return tempX;
 //    }
 
-//    public static float methodlegandra(float a, float b) {
-//        float[] x = new float[]{-0.83603f, 0.83603f, -0.96816f, 0.96816f, -0.32425f};
-//        float[] x3 = new float[]{-0.9324700f, -0.6612094f, -0.238614f, 0.2386142f, 0.6612094f, 0.9324700f};
-//        float[] ai2 = new float[]{0.1713245f, 0.3607616f, 0.4679140f, 0.4679140f, 0.3607616f, 0.1713245f};
-//        float sum = 0;
-//        float[] x2 = new float[x.length];
-//        float[] ai = new float[x.length];
-//        for (int i = 0; i < x.length; i++) {
-//            x2[i] = a + ((x[i] + 1) * (a - b)) / 2;
-//            ai[i] = (float) (2 / (1 - Math.pow(x3[i], 2) + Math.pow(dlegandra6(x3[i]), 2)));   //<---
-//        }
-//        for (int i = 0; i < x.length; i++) {
-//            sum += ai2[i] * function1(x3[i]);         // <---
-//        }
-//        return (a - b) / 10 * (sum);
-//    }
-
-
     public static float methodlegandra(float a, float b) {
-        float[] x3 = new float[]{-0.9324700f, -0.6612094f, -0.238614f, 0.2386142f, 0.6612094f, 0.9324700f};
-        float[] ai2 = new float[]{0.1713245f, 0.3607616f, 0.4679140f, 0.4679140f, 0.3607616f, 0.1713245f};
+        float[] x = new float[]{-0.96816f, -0.836031f, -0.613371f, -0.324253f, 0, 0.324253f, 0.613371f, 0.836031f, 0.96816f};
         float sum = 0;
-        float ra = (b - a) / 2;
-        float su = (a + b) / 2;
-        for (int i = 0; i < x3.length; i++) {
-            sum += ai2[i] * function1(b - a / 2 * x3[i] + a + b / 2);
+        float[] ai = new float[x.length];
+        for (int i = 0; i < x.length; i++) {
+            ai[i] = (float) (2 / ((1 - Math.pow(x[i], 2)) * Math.pow(legandra(x[i]), 2)));   //<---
+        }
+        for (int i = 0; i < x.length; i++) {
+            sum += ai[i] * function1((b - a) / 2 * x[i] + (b + a) / 2);         // <---
         }
         return (b - a) / 2 * (sum);
     }
 
 
-    public static float dlegandra9(float x) {
-        return (float) (45 / 128 * (7 - 308 * Math.pow(x, 2) + 2002 * Math.pow(x, 4) - 4004 * Math.pow(x, 6) + 2431 * Math.pow(x, 8)));
+//    public static float methodlegandra(float a, float b) {
+//        float[] x3 = new float[]{-0.9324700f, -0.6612094f, -0.238614f, 0.2386142f, 0.6612094f, 0.9324700f};
+//        float[] ai2 = new float[]{0.1713245f, 0.3607616f, 0.4679140f, 0.4679140f, 0.3607616f, 0.1713245f};
+//        float sum = 0;
+//        float ra = (b - a) / 2;
+//        float su = (a + b) / 2;
+//        for (int i = 0; i < x3.length; i++) {
+//            sum += ai2[i] * function1(b - a / 2 * x3[i] + a + b / 2);
+//        }
+//        return (b - a) / 2 * (sum);
+//    }
+
+
+    private static float legandra(float x) {
+        return (float) ((float) 45 / 128 * (7 - 308 * Math.pow(x, 2) + 2002 * Math.pow(x, 4) - 4004 * Math.pow(x, 6) + 2431 * Math.pow(x, 8)));
     }
 
     public static float dlegandra6(float x) {
